ae268024dbca1e82eeaab472c82f391569a0cda7
문태성/책임/D2C디지털마케팅팀 <ts.moon@lge.com>
2026-05-14 16:15:04 +0900

    Change SITEMAP_AGENT_MAX_URLS default to 50

--- sitemap_agent.py
+++ sitemap_agent.py
@@ -17,7 +17,7 @@ import db
 log = logging.getLogger("geo_audit.sitemap_agent")
 
 # 환경변수로 운영 중 튜닝 가능
-MAX_URLS = int(os.environ.get("SITEMAP_AGENT_MAX_URLS", 500))
+MAX_URLS = int(os.environ.get("SITEMAP_AGENT_MAX_URLS", 50))
 PER_URL_TIMEOUT = float(os.environ.get("SITEMAP_AGENT_URL_TIMEOUT", 60))
 SITEMAP_MAX_DEPTH = int(os.environ.get("SITEMAP_AGENT_MAX_DEPTH", 3))
 CONCURRENCY = int(os.environ.get("SITEMAP_AGENT_CONCURRENCY", 5))
